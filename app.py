3b0cfcc67a88bead7d2e68b957d84f797c0ce1f9
Mike <vmichele80@hotmail.com>
2026-03-09 18:32:22 +0100

    Added the templates for the different pages

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data
 db.init_app(app)
 
 
-
-
 # This is the code to generate the tables. Commented out after the first run
 """
 with app.app_context():
